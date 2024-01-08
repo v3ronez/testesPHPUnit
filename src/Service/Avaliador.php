24546eb827f3e358db12ce2c24057a1c0c8626f2
Henrique Veronez <v3ronez.dev@gmail.com>
2024-01-07 22:23:19 -0300

    aula 295

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -1,24 +1,41 @@
 <?php
 
-    namespace Alura\Leilao\Service;
+namespace Alura\Leilao\Service;
 
-    use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
 
-    class Avaliador
+class Avaliador
+{
+    private $maiorValor = 0;
+    private $maioresLances = [];
+
+    public function avalia(Leilao $leilao): void
     {
-        private $maiorValor = 0;
-
-        public function avalia(Leilao $leilao): void
-        {
-            foreach ($leilao->getLances() as $lance) {
-                if ($lance->getValor() > $this->maiorValor) {
-                    $this->maiorValor = $lance->getValor();
-                }
+        foreach ($leilao->getLances() as $lance) {
+            if ($lance->getValor() > $this->maiorValor) {
+                $this->maiorValor = $lance->getValor();
             }
         }
-
-        public function getMaiorValor(): float
-        {
-            return $this->maiorValor;
-        }
+        $lances = $leilao->getLances();
+        usort(
+            $lances,
+            fn(Lance $lance1, Lance $lance2) => $lance2->getValor()
+                - $lance1->getValor()
+        );
+        $this->maioresLances = array_slice($lances, 0, 3);
     }
+
+    public function getMaiorValor(): float
+    {
+        return $this->maiorValor;
+    }
+
+    /**
+     * @return Lance[]
+     */
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
+    }
+}
--- tests/Service/AvaliadorTest.php
+++ tests/Service/AvaliadorTest.php
@@ -1,54 +1,76 @@
 <?php
 
-    namespace Alura\Leilao\Tests\Service;
+namespace Alura\Leilao\Tests\Service;
 
-    use Alura\Leilao\Model\Lance;
-    use Alura\Leilao\Model\Leilao;
-    use Alura\Leilao\Model\Usuario;
-    use Alura\Leilao\Service\Avaliador;
-    use PHPUnit\Framework\TestCase;
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use Alura\Leilao\Service\Avaliador;
+use PHPUnit\Framework\TestCase;
 
-    class AvaliadorTest extends TestCase
+class AvaliadorTest extends TestCase
+{
+    public function testAvaliadorDeveValidarUltimoLance()
     {
-        public function testAvaliadorDeveValidarUltimoLance()
-        {
-            // Arrange - Given
-            $leilao = new Leilao('Fiat 147 0KM');
+        // Arrange - Given
+        $leilao = new Leilao('Fiat 147 0KM');
 
-            $maria = new Usuario('Maria');
-            $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('João');
 
-            $leilao->recebeLance(new Lance($joao, 2000));
-            $leilao->recebeLance(new Lance($maria, 2500));
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leilao->recebeLance(new Lance($maria, 2500));
 
-            $leiloeiro = new Avaliador();
-            // Act - When
-            $leiloeiro->avalia($leilao);
+        $leiloeiro = new Avaliador();
+        // Act - When
+        $leiloeiro->avalia($leilao);
 
-            $maiorValor = $leiloeiro->getMaiorValor();
+        $maiorValor = $leiloeiro->getMaiorValor();
 
-            // Assert - Then
-            $this->assertEquals(2500, $maiorValor);
-        }
-
-        public function testAvaliadorDeveValidarLance()
-        {
-            // Arrange - Given
-            $leilao = new Leilao('Fiat 147 0KM');
-
-            $maria = new Usuario('Maria');
-            $joao = new Usuario('João');
-
-            $leilao->recebeLance(new Lance($maria, 2500));
-            $leilao->recebeLance(new Lance($joao, 2000));
-
-            $leiloeiro = new Avaliador();
-            // Act - When
-            $leiloeiro->avalia($leilao);
-
-            $maiorValor = $leiloeiro->getMaiorValor();
-
-            // Assert - Then
-            $this->assertEquals(2500, $maiorValor);
-        }
+        // Assert - Then
+        $this->assertEquals(2500, $maiorValor);
     }
+
+    public function testAvaliadorDeveValidarLance()
+    {
+        // Arrange - Given
+        $leilao = new Leilao('Fiat 147 0KM');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('João');
+
+        $leilao->recebeLance(new Lance($maria, 2500));
+        $leilao->recebeLance(new Lance($joao, 2000));
+
+        $leiloeiro = new Avaliador();
+        // Act - When
+        $leiloeiro->avalia($leilao);
+
+        $maiorValor = $leiloeiro->getMaiorValor();
+
+        // Assert - Then
+        $this->assertEquals(2500, $maiorValor);
+    }
+
+    public function testAvaliadorPega3ultimosLances()
+    {
+        $leilao = new Leilao('Fiat sla');
+        $joao = new Usuario('joao');
+        $maria = new Usuario('maria');
+        $mario = new Usuario('mario');
+        $jorge = new Usuario('jorge');
+
+        $leilao->recebeLance(new Lance($joao, 1000));
+        $leilao->recebeLance(new Lance($maria, 500));
+        $leilao->recebeLance(new Lance($mario, 3400));
+        $leilao->recebeLance(new Lance($jorge, 4400));
+        $avaliador = new Avaliador();
+
+        $avaliador->avalia($leilao);
+        $maiores = $avaliador->getMaioresLances();
+        $this->assertCount(3, $maiores);
+        $this->assertEquals(4400, $maiores[0]->getValor());
+        $this->assertEquals(3400, $maiores[1]->getValor());
+        $this->assertEquals(1000, $maiores[2]->getValor());
+    }
+}
