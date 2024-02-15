b3381789731d9777b4587c04cd9f0efab0eea3d1
Henrique Veronez <v3ronez.dev@gmail.com>
2024-02-14 22:22:10 -0300

    teste integracao

---
+++ part_3_integration/tests/Integration/Dao/LeilaoDaoTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Alura\Leilao\Tests\Integration\Dao;
+
+use Alura\Leilao\Dao\Leilao as LeilaoDao;
+use Alura\Leilao\Infra\ConnectionCreator;
+use Alura\Leilao\Model\Leilao;
+use PHPUnit\Framework\TestCase;
+
+class LeilaoDaoTest extends TestCase
+{
+    public function testInsercaoEBuscaDevemFuncionar()
+    {
+        $leilao = new Leilao('Variante 0Km');
+        $leilaoDao = new LeilaoDao(ConnectionCreator::getConnection());
+
+        $leilaoDao->salva($leilao);
+        $leiloes = $leilaoDao->recuperarNaoFinalizados();
+
+        self::assertCount(1, $leiloes);
+        self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
+        self::assertSame(
+            'Viariante 0Km',
+            $leiloes[0]->recuperarDescricao()
+        );
+    }
+}
